a182da97b8fa1a8d7836e81504e4e092a21e1dcf
Tho-gaKo <thomas-gabriel.korak@coop.ch>
2023-12-12 14:33:24 +0100

    Initial commit

---
+++ src/main/java/com/example/sbbap/SbbApApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbbap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbbApApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbbApApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sbbap/SbbApApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbbap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbbApApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
