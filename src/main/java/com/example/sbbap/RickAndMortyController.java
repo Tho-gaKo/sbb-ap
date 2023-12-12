1a4054f583ead654e36d0db4a7b102c676b0120a
Tho-gaKo <thomas-gabriel.korak@coop.ch>
2023-12-12 15:53:53 +0100

    commit

--- src/main/java/com/example/sbbap/RickAndMortyController.java
+++ src/main/java/com/example/sbbap/RickAndMortyController.java
@@ -2,23 +2,30 @@ package com.example.sbbap;
 
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/rickandmorty")
 public class RickAndMortyController {
 
-    static class Character {
+    private final RickAndMortyService rickAndMortyService;
 
+    @GetMapping
+    public List<RickAndMortyCharacter> getAllCharacters() {
+        return rickAndMortyService.getAllCharacters();
     }
 
-
-    @GetMapping("/characters")
+    @GetMapping("/characters/{id}")
+    public RickAndMortyCharacter getCharacterById(@PathVariable int id) {
+        return rickAndMortyService.getCharacterById(id);
+    }
 
 
 
--- src/main/java/com/example/sbbap/RickAndMortyService.java
+++ src/main/java/com/example/sbbap/RickAndMortyService.java
@@ -1,12 +1,38 @@
 package com.example.sbbap;
 
 
-import org.springframework.boot.autoconfigure.web.reactive.function.client.WebClientAutoConfiguration;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
 
 @Service
 public class RickAndMortyService {
     private static final String rickAndMortyApi = "https://rickandmortyapi.com/";
 
-    private final WebClient
+    private WebClient webClient;
+
+    public RickAndMortyService(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+   public RickAndMortyCharacter getCharacterById(int characterId)  {
+        return webClient.get()
+                .uri("/character/{id}", characterId)
+                .retrieve()
+                .toEntity(RickAndMortyCharacter.class)
+                .block()
+                .getBody();
+   }
+
+   public List<RickAndMortyCharacter> getAllCharacters() {
+        return webClient.get()
+                .uri("/character")
+                .retrieve()
+                .toEntityList(RickAndMortyCharacter.class)
+                .block()
+                .getBody();
+
+   }
 }
+
